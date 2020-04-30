ce5c2a4cb79d2b968459f64114f3f622c295345f
EuEric <ericabraham8@yahoo.com>
2020-04-30 19:47:04 +0200

    Info on mouse
    Added information on mouse hover about the points.

--- dbl project/public/scripts/d3.js
+++ dbl project/public/scripts/d3.js
@@ -1,5 +1,11 @@
 $.get("/data", (data) => {
   console.log(data);
+  //ADDS INVISIBLE DIV WHICH WILL BE USED TO OUTPUT EXTRA INFO FOR EACH POINT
+  var info = d3
+    .select("body")
+    .append("div")
+    .attr("class", "output")
+    .style("opacity", 0);
   const filteredData = data.filter(
     (value) => value.StimuliName == "01_Antwerpen_S1.jpg"
   );
@@ -16,7 +22,25 @@ $.get("/data", (data) => {
     .attr("cx", (row) => row.MappedFixationPointX)
     .attr("cy", (row) => row.MappedFixationPointY)
     .attr("r", 10)
-    .style("fill", "steelblue");
+    .style("fill", "steelblue")
+    .on("mouseover", function (data) {
+      info.transition().duration(200).style("opacity", "1");
+      info.html(
+        "x: " +
+          data.MappedFixationPointX +
+          "<br>" +
+          "y:" +
+          data.MappedFixationPointY +
+          "<br>" +
+          "User: " +
+          data.user
+      );
+      info.style("left", d3.event.pageX + 8 + "px");
+      info.style("top", d3.event.pageY - 80 + "px");
+    })
+    .on("mouseout", function (data) {
+      info.transition().duration(200).style("opacity", 0);
+    });
 });
 
 console.log("minion banana army is strong");
