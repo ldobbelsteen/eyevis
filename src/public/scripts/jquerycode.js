7ca750cdc628d694723afd62af530eb7cdc86cfa
Chiara <c.liotta@student.tue.nl>
2020-05-05 18:20:17 +0200

    minor change to animation bc of width

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         $div1.toggleClass('isOut')
         var isOut = $div1.hasClass('isOut')
         var margLeft = $(window).width() * 0.05 + 320
-        $div1.animate({marginLeft: isOut ? margLeft : '5vw'}, 450)
+        var newWidth = $(window).width() * 0.9 - 320
+        $div1.animate({width: isOut ? newWidth : '90vw'}, { duration: 450, queue: false})
+        $div1.animate({marginLeft: isOut ? margLeft : '5vw'}, { duration: 450, queue: false})
     })
 });
