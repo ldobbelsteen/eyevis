3cbfcc15d195ea62fcbc0b8857eb64dcb050daa6
Chiara <c.liotta@student.tue.nl>
2020-05-05 18:08:56 +0200

    minor changes + main content moves with menu

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $('b').click(function(){
-        $('#menu').toggle();
-    });
+    var $div1 = $('#main')
+    $('#menu').click(function() {
+        $div1.toggleClass('isOut')
+        var isOut = $div1.hasClass('isOut')
+        var margLeft = $(window).width() * 0.05 + 320
+        $div1.animate({marginLeft: isOut ? margLeft : '5vw'}, 450)
+    })
 });
