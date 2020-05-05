5ff2439e5cefb100856930f837c2da1d335bf1d7
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2020-05-05 11:41:56 +0200

    Put visualization into a function

--- src/public/scripts/visualization.js
+++ src/public/scripts/visualization.js
@@ -1,31 +1,32 @@
-var dataset = "metro"
-var stimuli = "03b_Bordeaux_S2.jpg"
-
-// Download csv file and convert to json
-Papa.parse("/data/datasets/" + dataset, {
-    download: true,
-    header: true,
-    complete: (results) => {
-        var filteredData = results.data.filter(
-            (value) => value.StimuliName == stimuli // Take data of single stimulus
-        )
-        var img = new Image() // Create stimulus image variable
-        img.onload = () => { // When the image has loaded, render the eyetracking data over it
-            d3.selectAll("#visualization")
-                .append("svg")
-                .attr("width", img.width)
-               .attr("height", img.height)
-                .style("background-image", `url(${img.src})`)
-                .append("g")
-                .selectAll("dot")
-                .data(filteredData)
-                .enter()
-                .append("circle")
-                .attr("cx", (row) => row.MappedFixationPointX)
-                .attr("cy", (row) => row.MappedFixationPointY)
-                .attr("r", 10)
-                .style("fill", "steelblue")
+function visualize(dataset, stimulus) {
+    Papa.parse("/data/datasets/" + dataset, {
+        download: true,
+        header: true,
+        complete: (result) => { 
+            var data = result.data.filter(
+                (value) => value.StimuliName == stimulus
+            )
+            var stim = new Image()
+            
+            stim.onload = () => {
+                d3.select("#visualization")
+                    .append("svg")
+                    .attr("width", stim.width)
+                    .attr("height", stim.height)
+                    .style("background-image", `url(${stim.src})`)
+                    .append("g")
+                    .selectAll("dot")
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", (row) => row.MappedFixationPointX)
+                    .attr("cy", (row) => row.MappedFixationPointY)
+                    .attr("r", 10)
+                    .style("fill", "steelblue")
+            }
+            stim.src = "/data/stimuli/" + stimulus
         }
-        img.src = "/data/stimuli/" + stimuli // Load the image
-    }
-})
+    })
+}
+
+visualize("metro", "03b_Bordeaux_S2.jpg")
