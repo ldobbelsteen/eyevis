9c8bd81e7272c743b77081447dfcb91e13f7a1cb
Chiara <c.liotta@student.tue.nl>
2020-05-05 20:09:52 +0200

    changes to menu style + clicking header takes back to main page

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     $('#menu').click(function() {
         $div1.toggleClass('isOut')
         var isOut = $div1.hasClass('isOut')
-        var margLeft = $(window).width() * 0.05 + 320
-        var newWidth = $(window).width() * 0.9 - 320
-        $div1.animate({width: isOut ? newWidth : '90vw'}, { duration: 450, queue: false})
-        $div1.animate({marginLeft: isOut ? margLeft : '5vw'}, { duration: 450, queue: false})
+        var margLeft = $(window).width() * 0.05 + 270
+        var newWidth = $(window).width() * 0.9 - 270
+        $div1.animate({width: isOut ? newWidth : '90vw'}, { duration: 550, queue: false})
+        $div1.animate({marginLeft: isOut ? margLeft : '5vw'}, { duration: 550, queue: false})
     })
 });
