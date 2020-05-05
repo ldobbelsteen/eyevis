c0b362f3f85df595a9b1b6dfb91ee9fd72c85177
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2020-05-05 13:34:01 +0200

    Create separate data fetch function

--- src/public/scripts/visualization.js
+++ src/public/scripts/visualization.js
@@ -1,48 +1,24 @@
 function visualize(dataset, stimulus) {
     Papa.parse("/data/datasets/" + dataset, {
+// Fetch dataset and put it into the global data variable
+// Also put all the stimuli in the dropdown menu
+function fetchDataset(dataset) {
+    Papa.parse("/data/datasets/" + dataset, { // Download and parse the dataset
         download: true,
         header: true,
-        complete: (result) => { 
-            var data = result.data.filter(
-                (value) => value.StimuliName == stimulus
-            )
-            var stim = new Image()
-            var info = d3
-                .select("body")
-                .append("div")
-                .attr("class", "info")
-                .style("opacity", 0)
-            
-            stim.onload = () => {
-                d3.select("#visualization")
-                    .append("svg")
-                    .attr("width", stim.width)
-                    .attr("height", stim.height)
-                    .style("background-image", `url(${stim.src})`)
-                    .append("g")
-                    .selectAll("dot")
-                    .data(data)
-                    .enter()
-                    .append("circle")
-                    .attr("cx", (row) => row.MappedFixationPointX)
-                    .attr("cy", (row) => row.MappedFixationPointY)
-                    .attr("r", 10)
-                    .style("fill", "steelblue")
-                    .on("mouseover", function (data) {
-                        info.transition().duration(200).style("opacity", "1")
-                        info.html(
-                            "x: " + data.MappedFixationPointX + "<br>" +
-                            "y: " + data.MappedFixationPointY + "<br>" +
-                            "User: " + data.user
-                        )
-                        info.style("left", d3.event.pageX + 8 + "px");
-                        info.style("top", d3.event.pageY - 80 + "px");
-                    })
-                    .on("mouseout", function (data) {
-                        info.transition().duration(200).style("opacity", 0);
-                    })
-            }
-            stim.src = "/data/stimuli/" + stimulus
+        complete: (result) => {
+            data = result.data // Write result to the global data variable
+            var menu = $("#stimuliMenu") // Select menu
+            menu.empty() // Empty any previous entries
+            menu.append($("<option disabled selected value> -- select a stimulus -- </option>")) // Default entry
+            var uniqueStimuli = [...new Set(data.map(item => item.StimuliName))] // List all unique stimuli
+            uniqueStimuli.forEach((element) => { // Add them to the stimuli menu
+                menu.append($("<option></option>").text(element))
+            })
+            menu.on("change", () => { // Visualize when stimulus has been selected
+                var selected = menu.children("option").filter(":selected").text()
+                visualize(selected)
+            })
         }
     })
 }
