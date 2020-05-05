4ebd90f7498d0541b29f0ddb705d5412ecaa820a
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2020-05-05 13:33:31 +0200

    Purify visualization function

--- src/public/scripts/visualization.js
+++ src/public/scripts/visualization.js
@@ -47,4 +47,44 @@ function visualize(dataset, stimulus) {
     })
 }
 
-visualize("metro", "03b_Bordeaux_S2.jpg")
+// Use the global dataset and the stimulus to visualize the data
+function visualize(stimulus) {
+    var filteredData = data.filter((value) => value.StimuliName == stimulus) // Filter data to stimulus
+    var stim = new Image() // Create image object for the stimulus
+    stim.onload = () => { // Run this once the image has loaded
+        var info = d3 // Info box
+            .select("body")
+            .append("div")
+            .attr("class", "info")
+            .style("opacity", 0)
+        d3.select("#visualization").selectAll("*").remove() // Clear any previous visualization
+        d3.select("#visualization") // Actually do the visualization
+            .append("svg")
+            .attr("width", stim.width)
+            .attr("height", stim.height)
+            .style("background-image", `url(${stim.src})`)
+            .append("g")
+            .selectAll("dot")
+            .data(filteredData)
+            .enter()
+            .append("circle")
+            .attr("cx", (row) => row.MappedFixationPointX)
+            .attr("cy", (row) => row.MappedFixationPointY)
+            .attr("r", 10)
+            .style("fill", "steelblue")
+            .on("mouseover", function (data) { // Show info box when mouse over
+                info.transition().duration(200).style("opacity", "1")
+                info.html(
+                    "x: " + data.MappedFixationPointX + "<br>" +
+                    "y: " + data.MappedFixationPointY + "<br>" +
+                    "User: " + data.user
+                )
+                info.style("left", d3.event.pageX + 8 + "px");
+                info.style("top", d3.event.pageY - 80 + "px");
+            })
+            .on("mouseout", function (data) { // Hide info box when mouse out
+                info.transition().duration(200).style("opacity", 0);
+            })
+    }
+    stim.src = "/data/stimuli/" + stimulus // Load the image
+}
