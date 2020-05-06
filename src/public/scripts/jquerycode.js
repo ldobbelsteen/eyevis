07eca538817d8debcd3feef725d933b88cd02dd9
Chiara <c.liotta@student.tue.nl>
2020-05-06 16:25:08 +0200

    made the two jquery functions interconnected

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -7,8 +7,12 @@ $(document).ready(function(){
         var moveRight = $main.hasClass('moveRight')
         var margLeft = $(window).width() * 0.05 + 270
         var smallerWidth = $(window).width() * 0.9 - 270
+        var hide = $("#about").hasClass('hide')
         $main.animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
         $main.animate({marginLeft: moveRight ? margLeft : '5vw'}, { duration: 250, queue: false})
+        if (!hide) {
+            $("#about").animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
+        }
     })
 
     $("#hide").click(function () {
@@ -16,8 +20,9 @@ $(document).ready(function(){
         $("#main-collapse").toggle();
         $("#about").toggleClass('hide')
         var hide = $("#about").hasClass('hide')
-        var margLeft = $(window).width() * 0.95 - 45
-        $("#about").animate({width: hide ? '45px' : '90vw'}, { duration: 0, queue: false})
+        var moveRight = $main.hasClass('moveRight')
+        var smallerWidth = $(window).width() * 0.9 - 270
+        $("#about").animate({width: hide ? '45px' : moveRight ? smallerWidth : '90vw' }, { duration: 0, queue: false})
         $("#about").toggleClass('float-right')
     });
 
