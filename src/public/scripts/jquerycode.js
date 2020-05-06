998549e9bf08d37e81ab0872bcb8b04787927bd6
Chiara <c.liotta@student.tue.nl>
2020-05-06 17:03:47 +0200

    menu doubleclick problem solved

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
 
-    // main body moves right or goes back to normal position depending on menu
     var $main = $('#main')
-    $('#menu').click(function() {
+    
+    //hide about section
+    $("#hide").click(function () {
+        $("#hidden1").toggle();
+        $("#main-collapse").toggle();
+        $("#about").toggleClass('hide')
+        var hide = $("#about").hasClass('hide')
+        var moveRight = $main.hasClass('moveRight')
+        var smallerWidth = $(window).width() * 0.9 - 270
+        $("#about").animate({width: hide ? '45px' : moveRight ? smallerWidth : '90vw' }, { duration: 0, queue: false})
+        $("#about").toggleClass('float-right')
+    });
+
+    // main body moves right or goes back to normal position depending on menu
+    $("#menu").on('click dblclick',function(e){
         $main.toggleClass('moveRight')
         var moveRight = $main.hasClass('moveRight')
         var margLeft = $(window).width() * 0.05 + 270
@@ -13,17 +26,6 @@ $(document).ready(function(){
         if (!hide) {
             $("#about").animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
         }
-    })
-
-    $("#hide").click(function () {
-        $("#hidden1").toggle();
-        $("#main-collapse").toggle();
-        $("#about").toggleClass('hide')
-        var hide = $("#about").hasClass('hide')
-        var moveRight = $main.hasClass('moveRight')
-        var smallerWidth = $(window).width() * 0.9 - 270
-        $("#about").animate({width: hide ? '45px' : moveRight ? smallerWidth : '90vw' }, { duration: 0, queue: false})
-        $("#about").toggleClass('float-right')
     });
 
 });
