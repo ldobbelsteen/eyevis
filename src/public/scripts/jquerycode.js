665256038bcd7c3d235d2b5afb674edb6166f30d
Chiara <c.liotta@student.tue.nl>
2020-05-06 16:12:26 +0200

    adjustments to hide button

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -4,25 +4,21 @@ $(document).ready(function(){
     var $main = $('#main')
     $('#menu').click(function() {
         $main.toggleClass('moveRight')
-        var isExtended = $main.width() >= $(window).width() * 0.9 - 270
         var moveRight = $main.hasClass('moveRight')
         var margLeft = $(window).width() * 0.05 + 270
         var smallerWidth = $(window).width() * 0.9 - 270
-        if (isExtended) {
-            $main.animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
-            $main.animate({marginLeft: moveRight ? margLeft : '5vw'}, { duration: 250, queue: false})
-        }
+        $main.animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
+        $main.animate({marginLeft: moveRight ? margLeft : '5vw'}, { duration: 250, queue: false})
     })
 
     $("#hide").click(function () {
         $("#hidden1").toggle();
         $("#main-collapse").toggle();
-        $main.toggleClass('hide')
-        var hide = $main.hasClass('hide')
+        $("#about").toggleClass('hide')
+        var hide = $("#about").hasClass('hide')
         var margLeft = $(window).width() * 0.95 - 45
-        $main.animate({width: hide ? '45px' : '90vw'}, { duration: 0, queue: false})
-        $main.animate({marginLeft: hide ? margLeft : '5vw'}, { duration: 0, queue: false})
-        
+        $("#about").animate({width: hide ? '45px' : '90vw'}, { duration: 0, queue: false})
+        $("#about").toggleClass('float-right')
     });
 
 });
