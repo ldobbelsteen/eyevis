c49a9c3f0ff61fe27aac167fa4f63501dacc3455
Chiara <c.liotta@student.tue.nl>
2020-05-06 17:46:18 +0200

    supports window resizing

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
         var moveRight = $main.hasClass('moveRight')
         var smallerWidth = $(window).width() * 0.9 - 270
         $("#about").animate({width: hide ? '45px' : moveRight ? smallerWidth : '90vw' }, { duration: 0, queue: false})
+        $("#about").animate({padding: hide ? '0 0 0 0' : '5px 20px 10px 20px' }, { duration: 0, queue: false})
         $("#about").toggleClass('float-right')
     });
 
@@ -27,5 +28,18 @@ $(document).ready(function(){
             $("#about").animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
         }
     });
+
+    window.onresize = function(event) {
+        var hide = $("#about").hasClass('hide')
+        var moveRight = $main.hasClass('moveRight')
+        var smallerWidth = $(window).width() * 0.9 - 270
+        var margLeft = $(window).width() * 0.05 + 270
+        if (!hide) {
+            $("#about").animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
+        }
+        $("#about").animate({width: hide ? '45px' : moveRight ? smallerWidth : '90vw' }, { duration: 0, queue: false})
+        $main.animate({width: moveRight ? smallerWidth : '90vw'}, { duration: 250, queue: false})
+        $main.animate({marginLeft: moveRight ? margLeft : '5vw'}, { duration: 250, queue: false})
+    }
 
 });
