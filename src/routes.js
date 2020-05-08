90b69ae6fcc9bbed91b8d34d6d980194def642ef
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2020-05-08 15:06:53 +0200

    Revert "Formatting code, made fs async and fixed exports"
    This reverts commit ac5c5594169867440a77df3a746fd91e1321f9e6.

--- src/routes.js
+++ src/routes.js
@@ -1,38 +1,35 @@
-var data = require("./data");
-var upload = require("express-fileupload");
-var routes = require("express").Router();
+var data = require("./data")
+var upload = require("express-fileupload")
+var routes = require("express").Router()
 
-console.log(data);
 // Serve main page
 routes.get("/", (req, res) => {
-  res.render("index");
-});
+    res.render("index")
+})
 
 // Serve list of datasets
 routes.get("/datasets", (req, res) => {
-  var datasets = data.listDatasets();
-  res.send(datasets);
-});
+    var datasets = data.listDatasets()
+    res.send(datasets)
+})
 
 // Serve list of stimuli
 routes.get("/stimuli", (req, res) => {
-  var stimuli = data.listStimuli();
-  res.send(stimuli);
-});
+    var stimuli = data.listStimuli()
+    res.send(stimuli)
+})
 
 // Enable file uploads
-routes.use(
-  upload({
-    limits: { fileSize: 128 * 1024 * 1024 },
-  })
-);
+routes.use(upload({
+    limits: { fileSize: 128 * 1024 * 1024 }
+}))
 
 // Handle file uploads
 routes.post("/upload", (req, res) => {
-  var name = req.files.upload.name;
-  var buffer = req.files.upload.data;
-  data.addDataset(name, buffer);
-  res.render("upload");
-});
+    var name = req.files.upload.name
+    var buffer = req.files.upload.data
+    data.addDataset(name, buffer)
+    res.render("upload")
+})
 
-module.exports = routes;
+module.exports = routes
