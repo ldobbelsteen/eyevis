ae1518d358fc3d09c7df4c38c2397af37d45ed0a
ldobbelsteen <l.dobbelsteen@gmail.com>
2020-05-26 12:51:17 +0200

    Make use of D3 color scale instead of pre-defined colors

--- src/public/modules/visFive.js
+++ src/public/modules/visFive.js
@@ -59,19 +59,8 @@ export function visualize() {
 
     function updateTimelines() {
 
-        // Array of random colors
-        let colors = [
-            "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177", "#0d5ac1",
-            "#f205e6", "#1c0365", "#14a9ad", "#4ca2f9", "#a4e43f", "#d298e2", "#6119d0",
-            "#d2737d", "#c0a43c", "#f2510e", "#651be6", "#79806e", "#61da5e", "#cd2f00",
-            "#9348af", "#01ac53", "#c5a4fb", "#996635", "#b11573", "#4bb473", "#75d89e",
-            "#2f3f94", "#2f7b99", "#da967d", "#34891f", "#b0d87b", "#ca4751", "#7e50a8",
-            "#c4d647", "#e0eeb8", "#11dec1", "#289812", "#566ca0", "#ffdbe1", "#2f1179",
-            "#935b6d", "#916988", "#513d98", "#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
-            "#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977",
-            "#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b",
-            "#5be4f0", "#57c4d8", "#a4d17a", "#225b80", "#be608b", "#96b00c", "#088baf"
-        ];
+        // Make a scale of colors
+        let colors = d3.interpolateCool;
 
         // Get input values for the amount of horizontal and vertical AOIs
         let gridSizeX = gridSizeInputX.val();
@@ -88,12 +77,12 @@ export function visualize() {
         for (let x = 0; x < gridSizeX; x++) {
             for (let y = 0; y < gridSizeY; y++) {
                 AOIs.push({
-                    color: colors.pop(),
+                    color: colors(Math.random()),
                     x1: AOIsizeX * x,
                     x2: AOIsizeX * (x + 1),
                     y1: AOIsizeY * y,
                     y2: AOIsizeY * (y + 1)
-                })
+                });
             }
         }
 
