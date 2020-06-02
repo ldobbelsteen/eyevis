4bdff2aa2498d0bb9b3095f5c1f743fe2a1678f5
Tristan Tummers <t.tummers@student.tue.nl>
2020-06-02 12:33:26 +0200

    Data ready for sankey diagram (nodes and links)

--- src/public/modules/visTwo.js
+++ src/public/modules/visTwo.js
@@ -6,6 +6,12 @@ const gridSizeInputY = $("#vis-two input:eq(1)");
 var filteredData;
 var gridSizeX;
 var gridSizeY;
+var imgHeight;
+var imgWidth;
+var img, svg;
+
+var AOIs;
+var gazeToAOI;
 
 // Update data to appriopate stimuli
 function updateData() {
@@ -27,7 +33,12 @@ function updateData() {
     });
 }
 
-function createAOIs(let sizeX, let sizeY, let stimulusWidth, let stimulusHeight) {
+function compare(a, b) {
+    return a.Timestamp - b.Timestamp;
+}
+
+// define the areas of interest
+function createAOIs(sizeX, sizeY, stimulusWidth, stimulusHeight) {
     let arrayAOI = [];
 
     let AOIsizeX = stimulusWidth / sizeX;
@@ -49,37 +60,93 @@ function createAOIs(let sizeX, let sizeY, let stimulusWidth, let stimulusHeight)
     return arrayAOI;
 }
 
-function gazeAOI(let data, let arrayAOI) {
+// determine in which AOI the gazes are.
+// function returns array of timestamp,user,aoi
+function gazeAOI(data, arrayAOI) {
     let arrayGazeAOI = []
 
-    data.forEach(fixation => {
+    data.forEach(gaze => {
         let x = gaze.MappedFixationPointX;
         let y = gaze.MappedFixationPointY;
 
         arrayAOI.forEach(aoi => {
             if (x >= aoi.x && x <= aoi.x1 && y >= aoi.y && y <= aoi.y1) {
                 arrayGazeAOI.push({
-                    timestamp:
-                })
+                    timestamp: gaze.Timestamp,
+                    user: gaze.user,
+                    aoi: aoi
+                });
             }
         });
     });
 
+    return arrayGazeAOI;
+}
 
+// determine
+function transitionsAOI(filteredGaze) {
+    let result = [];
+
+    let users = [...new Set(data.map((item) => item.user))];
+    users.forEach(user => {
+        let userData = filterData(filteredGaze, {
+            user: user
+        });
+
+        let sortedData = userData.sort(compare);
+
+        for (let i = 0; i < sortedData.length; i++) {
+            result.push({
+                user: user,
+                start: sortedData[i].aoi,
+                end: sortedData[i+1].aoi
+            });
+        }
+    });
+
+    return result;
+
+}
+
+
+
+// function visualizeGrid() {
+//     let gridX = svg.selectAll(".gridX")
+//         .data(AOIs)
+//         .enter().append("g")
+//         .attr("class", "gridX");
+//
+//     let gridY = svg.selectAll(".gridY")
+//         .data(function(d) {return d;})
+//         .enter().append("rect")
+//         .attr("class", "gridY")
+//         .attr("x", function(d) {return d.x;})
+//         .attr("y", function(d) {return d.y;})
+//         .attr("width", function(d) {return d.width;})
+//         .attr("height", function(d) {return d.height;})
+//         .style("stroke",  "#222")
+// }
+
+function visuaizeSankey() {
+    return true;
 }
 
 export function initialize() {
     updateData();
 
-    let AOIs = createAOIs(gridSizeX, gridSizeY, imgWidth, imgHeight);
+    AOIs = createAOIs(gridSizeX, gridSizeY, imgWidth, imgHeight);
+
+    gazeToAOI = gazeAOI(filteredData, AOIs);
+
+    transistions = transitionsAOI(gazeToAOI);
 
 }
 
-function visualize() {
+export function visualize() {
     // Empty container
     container.html("");
 
-    let svg = container.append("svg");
+    svg = container.append("svg");
     gridSizeX = gridSizeInputX.val();
     gridSizeY = gridSizeInputY.val();
 
@@ -95,31 +162,10 @@ function visualize() {
             .attr("height", imgHeight)
             .attr("xlink:href", `/stimuli/${window.stimulus}`)
             .attr("class", "img");
-        visualizeGrid();
-        visualizeSankey();
+        // visualizeGrid();
+        // visualizeSankey();
     };
     img.src = `/stimuli/${window.stimulus}`;
-
-    function visualizeGrid() {
-        let gridX = svg.selectAll(".gridX")
-            .data(AOIs)
-            .enter().append("g")
-            .attr("class", "gridX");
-
-        let gridY = svg.selectAll(".gridY")
-            .data(function(d) {return d;})
-            .enter().append("rect")
-            .attr("class", "gridY")
-            .attr("x", function(d) {return d.x;})
-            .attr("y", function(d) {return d.y;})
-            .attr("width", function(d) {return d.width;})
-            .attr("height", function(d) {return d.height;})
-            .style("stroke",  "#222")
-    }
-
-    function visuaizeSankey() {
-
-    }
 
 
 }
