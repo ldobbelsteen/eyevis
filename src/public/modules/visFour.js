0084c910dea3b63a768e123d2aef0953fcaea2df
Chiara <c.liotta@student.tue.nl>
2020-06-10 17:38:24 +0200

    small menu restructuring to add AOI tab

--- src/public/modules/visFour.js
+++ src/public/modules/visFour.js
@@ -162,7 +162,7 @@ function overlayData() {
                 .style('fill', 'white')
                 .style("text-anchor", "middle")
                 .text("Density scale");
-                
+
     topInfo.append("g")
             .attr("transform", "translate("+ (containerW*0.15) +","+ 50 +")")
             .attr("class", "axis")
@@ -223,7 +223,7 @@ function showOverlay() {
                         pop.transition().duration(100).style("opacity", "1");
                         pop.html(
                             "<strong>density:</strong> " +
-                                densityData.value 
+                                densityData.value.toFixed(3) 
                         );
                         pop.style("left", d3.event.pageX + 8 + "px");
                         pop.style("top", d3.event.pageY - 40 + "px");
