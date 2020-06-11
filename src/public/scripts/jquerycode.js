8789eb10e0b08a3abc4fbb87863fba35ca16cc65
Chiara <c.liotta@student.tue.nl>
2020-06-11 14:37:09 +0200

    header style and responsiveness + no hidden upload section

--- src/public/scripts/jquerycode.js
+++ src/public/scripts/jquerycode.js
@@ -12,26 +12,6 @@ $(document).ready(function(){
     var $about = $("#about")
     var standardW = '90vw' //of main/about section
     var standardMarg = '5vw' // of main/about section
-    var hiddenW = '38px' //width when about section is hidden
-    var standardPadding = '5px 20px 10px 20px' //of about section
-    var hiddenPadding = '0 0 0 0' //of about section when hidden
-    
-    //hide about section
-    $("#hide").click(function () {
-        $("#hidden1").toggle(); // "hide" text in button
-        $("#main-collapse").toggle(); // text in section
-        $about.toggleClass('hide')
-        var hide = $about.hasClass('hide')
-        var moveRight = $main.hasClass('moveRight')
-        var smallerWidth = (window.innerWidth - 250) * 0.9
-        $about.animate({width: hide ? hiddenW : moveRight ? smallerWidth : standardW }, { duration: 0, queue: false})
-        hide ? $about.css('padding', hiddenPadding) : $about.css('padding', standardPadding)
-        hide ? $about.css('margin-bottom', '20px') : $about.css('margin-bottom', '0')
-        hide ? $about.css('position', 'absolute') : $about.css('position', 'relative')
-        hide ? $about.css('right', '0') : $about.css('right', 'auto')
-        hide ? $about.css('top', '107.6px') : $about.css('top', 'auto')
-        hide ? $about.css('border-top-right-radius', '0') : $about.css('border-top-right-radius', '4px')
-    });
 
     // main body moves right or goes back to normal position depending on menu
     $menu.on('click dblclick',function(){
@@ -45,9 +25,6 @@ $(document).ready(function(){
         var moveRight = $main.hasClass('moveRight')
         var margLeft = 250 + (window.innerWidth - 250) * 0.05
         var smallerWidth = (window.innerWidth - 250) * 0.9
-        if (!hide) {
-            $about.animate({width: moveRight ? smallerWidth : standardW}, { duration: 250, queue: false})
-        }
         $main.animate({width: moveRight ? smallerWidth : standardW}, { duration: 250, queue: false})
         $main.animate({marginLeft: moveRight ? margLeft : standardMarg}, { duration: 250, queue: false})
     }
@@ -155,7 +132,6 @@ $(document).ready(function(){
         $("#visualization").hide();
         $("#visualizationGrid").show();
         $("#export").show();
-        $(".grid-container").css("grid-template-columns", "auto auto")
     });
 
     
